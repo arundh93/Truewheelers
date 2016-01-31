eedafea1660d024991a3d566e0d6ea6dcf005dab
arun <arun>
2016-01-31 20:35:58 +0530

    intial boiler plate and added .gitignore

---
+++ src/Truewheelers/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Truewheelers.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
