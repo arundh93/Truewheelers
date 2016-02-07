9f05ecdbdb0f677b357466e3a25c40ce689841d9
arun <arun>
2016-02-07 21:14:04 +0530

    Added view model for shopping cart and modified the shopping cart view to show product details

---
+++ src/Truewheelers/ViewModels/ShoppingCart/ShoppingCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Truewheelers.Models;
+
+namespace Truewheelers.ViewModels.ShoppingCart
+{
+    public class ShoppingCartViewModel
+    {
+        public IEnumerable<Bicycles> model { get; set; }
+    }
+}
